149f1d46d26f9c350cd3e1c8e70c19825323077a
DragonWen <18475536452@163.com>
2020-07-03 16:57:10 +0800

    Excel支持dictType读取字符串组内容

--- fuwah-system/src/main/java/com/fuwah/system/utils/DictUtils.java
+++ fuwah-system/src/main/java/com/fuwah/system/utils/DictUtils.java
@@ -17,7 +17,12 @@ import java.util.List;
  * @version: $
  */
 @Component
-public class DictUtils {
+public class DictUtils
+{
+    /**
+     * 分隔符
+     */
+    public static final String SEPARATOR = ",";
 
     /**
      * 设置字典缓存
@@ -25,7 +30,8 @@ public class DictUtils {
      * @param key 参数键
      * @param dictDatas 字典数据列表
      */
-    public static void setDictCache(String key, List<SysDictData> dictDatas) {
+    public static void setDictCache(String key, List<SysDictData> dictDatas)
+    {
         CacheUtils.put(getCacheName(), getCacheKey(key), dictDatas);
     }
 
@@ -35,7 +41,8 @@ public class DictUtils {
      * @param key 参数键
      * @return dictDatas 字典数据列表
      */
-    public static List<SysDictData> getDictCache(String key) {
+    public static List<SysDictData> getDictCache(String key)
+    {
         Object cacheObj = CacheUtils.get(getCacheName(), getCacheKey(key));
         if (StringUtils.isNotNull(cacheObj))
         {
@@ -46,9 +53,114 @@ public class DictUtils {
     }
 
     /**
+     * 根据字典类型和字典值获取字典标签
+     *
+     * @param dictType 字典类型
+     * @param dictValue 字典值
+     * @return 字典标签
+     */
+    public static String getDictLabel(String dictType, String dictValue)
+    {
+        return getDictLabel(dictType, dictValue, SEPARATOR);
+    }
+
+    /**
+     * 根据字典类型和字典标签获取字典值
+     *
+     * @param dictType 字典类型
+     * @param dictLabel 字典标签
+     * @return 字典值
+     */
+    public static String getDictValue(String dictType, String dictLabel)
+    {
+        return getDictValue(dictType, dictLabel, SEPARATOR);
+    }
+
+    /**
+     * 根据字典类型和字典值获取字典标签
+     *
+     * @param dictType 字典类型
+     * @param dictValue 字典值
+     * @param separator 分隔符
+     * @return 字典标签
+     */
+    public static String getDictLabel(String dictType, String dictValue, String separator)
+    {
+        StringBuilder propertyString = new StringBuilder();
+        List<SysDictData> datas = getDictCache(dictType);
+
+        if (StringUtils.containsAny(separator, dictValue) && StringUtils.isNotEmpty(datas))
+        {
+            for (SysDictData dict : datas)
+            {
+                for (String value : dictValue.split(separator))
+                {
+                    if (value.equals(dict.getDictValue()))
+                    {
+                        propertyString.append(dict.getDictLabel() + separator);
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (SysDictData dict : datas)
+            {
+                if (dictValue.equals(dict.getDictValue()))
+                {
+                    return dict.getDictLabel();
+                }
+            }
+        }
+        return StringUtils.stripEnd(propertyString.toString(), separator);
+    }
+
+    /**
+     * 根据字典类型和字典标签获取字典值
+     *
+     * @param dictType 字典类型
+     * @param dictLabel 字典标签
+     * @param separator 分隔符
+     * @return 字典值
+     */
+    public static String getDictValue(String dictType, String dictLabel, String separator)
+    {
+        StringBuilder propertyString = new StringBuilder();
+        List<SysDictData> datas = getDictCache(dictType);
+
+        if (StringUtils.containsAny(separator, dictLabel) && StringUtils.isNotEmpty(datas))
+        {
+            for (SysDictData dict : datas)
+            {
+                for (String label : dictLabel.split(separator))
+                {
+                    if (label.equals(dict.getDictLabel()))
+                    {
+                        propertyString.append(dict.getDictValue() + separator);
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (SysDictData dict : datas)
+            {
+                if (dictLabel.equals(dict.getDictLabel()))
+                {
+                    return dict.getDictValue();
+                }
+            }
+        }
+        return StringUtils.stripEnd(propertyString.toString(), separator);
+    }
+
+    /**
      * 清空字典缓存
      */
-    public static void clearDictCache() {
+    public static void clearDictCache()
+    {
         CacheUtils.removeAll(getCacheName());
     }
 
@@ -57,7 +169,8 @@ public class DictUtils {
      *
      * @return 缓存名
      */
-    public static String getCacheName() {
+    public static String getCacheName()
+    {
         return Constants.SYS_DICT_CACHE;
     }
 
@@ -67,7 +180,8 @@ public class DictUtils {
      * @param configKey 参数键
      * @return 缓存键key
      */
-    public static String getCacheKey(String configKey) {
+    public static String getCacheKey(String configKey)
+    {
         return Constants.SYS_DICT_KEY + configKey;
     }
 }
