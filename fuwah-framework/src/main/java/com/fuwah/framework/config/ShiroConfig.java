a066533d6adce10aea0a5e1a31e243477a5cd45e
DragonWen <18475536452@163.com>
2020-07-07 16:27:27 +0800

    国家信息安全漏洞（请务必保持cipherKey密钥唯一性）

--- fuwah-framework/src/main/java/com/fuwah/framework/config/ShiroConfig.java
+++ fuwah-framework/src/main/java/com/fuwah/framework/config/ShiroConfig.java
@@ -85,6 +85,10 @@ public class ShiroConfig
     @Value("${shiro.cookie.maxAge}")
     private int maxAge;
 
+    // 设置cipherKey密钥
+    @Value("${shiro.cookie.cipherKey}")
+    private String cipherKey;
+
     // 登录地址
     @Value("${shiro.user.loginUrl}")
     private String loginUrl;
@@ -327,7 +331,7 @@ public class ShiroConfig
     {
         CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
         cookieRememberMeManager.setCookie(rememberMeCookie());
-        cookieRememberMeManager.setCipherKey(Base64.decode("fCq+/xW488hMTCD+cmJ3aQ=="));
+        cookieRememberMeManager.setCipherKey(Base64.decode(cipherKey));
         return cookieRememberMeManager;
     }
 
