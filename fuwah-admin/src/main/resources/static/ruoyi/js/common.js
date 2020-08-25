861e6b1bdddc592ac31b3bd1b52dc47532582904
DragonWen <18475536452@163.com>
2020-08-25 21:56:24 +0800

    openTab打开时滚动到当前页签

--- fuwah-admin/src/main/resources/static/ruoyi/js/common.js
+++ fuwah-admin/src/main/resources/static/ruoyi/js/common.js
@@ -1,14 +1,14 @@
 /**
  * 通用方法封装处理
- * Copyright (c) 2019 ruoyi 
+ * Copyright (c) 2019 ruoyi
  */
 $(function() {
-	
+
 	//  layer扩展皮肤
 	if (window.layer !== undefined) {
 		layer.config({
-		    extend: 'moon/style.css',
-		    skin: 'layer-ext-moon'
+			extend: 'moon/style.css',
+			skin: 'layer-ext-moon'
 		});
 	}
 
@@ -19,22 +19,22 @@ $(function() {
 
 	// select2复选框事件绑定
 	if ($.fn.select2 !== undefined) {
-        $.fn.select2.defaults.set( "theme", "bootstrap" );
+		$.fn.select2.defaults.set( "theme", "bootstrap" );
 		$("select.form-control:not(.noselect2)").each(function () {
 			$(this).select2().on("change", function () {
 				$(this).valid();
 			})
 		})
 	}
-	
+
 	// iCheck单选框及复选框事件绑定
 	if ($.fn.iCheck !== undefined) {
 		$(".check-box:not(.noicheck),.radio-box:not(.noicheck)").each(function() {
-            $(this).iCheck({
-                checkboxClass: 'icheckbox-blue',
-                radioClass: 'iradio-blue',
-            })
-        })
+			$(this).iCheck({
+				checkboxClass: 'icheckbox-blue',
+				radioClass: 'iradio-blue',
+			})
+		})
 	}
 
 	// 气泡弹出框特效（移到元素时）
@@ -64,45 +64,46 @@ $(function() {
 	// laydate 时间控件绑定
 	if ($(".select-time").length > 0) {
 		layui.use('laydate', function() {
-		    var laydate = layui.laydate;
-		    var startDate = laydate.render({
-		        elem: '#startTime',
-		        max: $('#endTime').val(),
-		        theme: 'molv',
-		        trigger: 'click',
-		        done: function(value, date) {
-		            // 结束时间大于开始时间
-		            if (value !== '') {
-		                endDate.config.min.year = date.year;
-		                endDate.config.min.month = date.month - 1;
-		                endDate.config.min.date = date.date;
-		            } else {
-		                endDate.config.min.year = '';
-		                endDate.config.min.month = '';
-		                endDate.config.min.date = '';
-		            }
-		        }
-		    });
-		    var endDate = laydate.render({
-		        elem: '#endTime',
-		        min: $('#startTime').val(),
-		        theme: 'molv',
-		        trigger: 'click',
-		        done: function(value, date) {
-		            // 开始时间小于结束时间
-		            if (value !== '') {
-		                startDate.config.max.year = date.year;
-		                startDate.config.max.month = date.month - 1;
-		                startDate.config.max.date = date.date;
-		            } else {
+			var laydate = layui.laydate;
+			var startDate = laydate.render({
+				elem: '#startTime',
+				max: $('#endTime').val(),
+				theme: 'molv',
+				trigger: 'click',
+				done: function(value, date) {
+					// 结束时间大于开始时间
+					if (value !== '') {
+						endDate.config.min.year = date.year;
+						endDate.config.min.month = date.month - 1;
+						endDate.config.min.date = date.date;
+					} else {
+						endDate.config.min.year = '';
+						endDate.config.min.month = '';
+						endDate.config.min.date = '';
+					}
+				}
+			});
+			var endDate = laydate.render({
+				elem: '#endTime',
+				min: $('#startTime').val(),
+				theme: 'molv',
+				trigger: 'click',
+				done: function(value, date) {
+					// 开始时间小于结束时间
+					if (value !== '') {
+						startDate.config.max.year = date.year;
+						startDate.config.max.month = date.month - 1;
+						startDate.config.max.date = date.date;
+					} else {
 						startDate.config.max.year = '2099';
 						startDate.config.max.month = '12';
 						startDate.config.max.date = '31';
-		            }
-		        }
-		    });
+					}
+				}
+			});
 		});
 	}
+
 	// laydate time-input 时间控件绑定
 	if ($(".time-input").length > 0) {
 		layui.use('laydate', function () {
@@ -150,46 +151,92 @@ $(function() {
 			});
 		});
 	}
+
 	// tree 关键字搜索绑定
 	if ($("#keyword").length > 0) {
 		$("#keyword").bind("focus", function focusKey(e) {
-		    if ($("#keyword").hasClass("empty")) {
-		        $("#keyword").removeClass("empty");
-		    }
+			if ($("#keyword").hasClass("empty")) {
+				$("#keyword").removeClass("empty");
+			}
 		}).bind("blur", function blurKey(e) {
-		    if ($("#keyword").val() === "") {
-		        $("#keyword").addClass("empty");
-		    }
-		    $.tree.searchNode(e);
+			if ($("#keyword").val() === "") {
+				$("#keyword").addClass("empty");
+			}
+			$.tree.searchNode(e);
 		}).bind("input propertychange", $.tree.searchNode);
 	}
+
 	// tree表格树 展开/折叠
 	var expandFlag;
 	$("#expandAllBtn").click(function() {
 		var dataExpand = $.common.isEmpty(table.options.expandAll) ? true : table.options.expandAll;
 		expandFlag = $.common.isEmpty(expandFlag) ? dataExpand : expandFlag;
-	    if (!expandFlag) {
-	    	$.bttTable.bootstrapTreeTable('expandAll');
-	    } else {
-	    	$.bttTable.bootstrapTreeTable('collapseAll');
-	    }
-	    expandFlag = expandFlag ? false: true;
+		if (!expandFlag) {
+			$.bttTable.bootstrapTreeTable('expandAll');
+		} else {
+			$.bttTable.bootstrapTreeTable('collapseAll');
+		}
+		expandFlag = expandFlag ? false: true;
 	})
+
 	// 按下ESC按钮关闭弹层
 	$('body', document).on('keyup', function(e) {
-	    if (e.which === 27) {
-	        $.modal.closeAll();
-	    }
+		if (e.which === 27) {
+			$.modal.closeAll();
+		}
 	});
 });
 
+(function ($) {
+	'use strict';
+	$.fn.toTop = function(opt) {
+		var elem = this;
+		var win = $(window);
+		var doc = $('html, body');
+		var options = $.extend({
+			autohide: true,
+			offset: 50,
+			speed: 500,
+			position: true,
+			right: 15,
+			bottom: 5
+		}, opt);
+		elem.css({
+			'cursor': 'pointer'
+		});
+		if (options.autohide) {
+			elem.css('display', 'none');
+		}
+		if (options.position) {
+			elem.css({
+				'position': 'fixed',
+				'right': options.right,
+				'bottom': options.bottom,
+			});
+		}
+		elem.click(function() {
+			doc.animate({
+				scrollTop: 0
+			}, options.speed);
+		});
+		win.scroll(function() {
+			var scrolling = win.scrollTop();
+			if (options.autohide) {
+				if (scrolling > options.offset) {
+					elem.fadeIn(options.speed);
+				} else elem.fadeOut(options.speed);
+			}
+		});
+	};
+})(jQuery);
+
 /** 刷新选项卡 */
 var refreshItem = function(){
-    var topWindow = $(window.parent.document);
+	var topWindow = $(window.parent.document);
 	var currentId = $('.page-tabs-content', topWindow).find('.active').attr('data-id');
 	var target = $('.RuoYi_iframe[data-id="' + currentId + '"]', topWindow);
-    var url = target.attr('src');
-    target.attr('src', url).ready();
+	var url = target.attr('src');
+	target.attr('src', url).ready();
 }
 
 /** 关闭选项卡 */
@@ -208,10 +255,10 @@ var closeItem = function(dataId){
 	if($.common.isNotEmpty(panelUrl)){
 		$('.menuTab[data-id="' + panelUrl + '"]', topWindow).addClass('active').siblings('.menuTab').removeClass('active');
 		$('.mainContent .RuoYi_iframe', topWindow).each(function() {
-            if ($(this).data('id') == panelUrl) {
-                $(this).show().siblings('.RuoYi_iframe').hide();
-                return false;
-            }
+			if ($(this).data('id') == panelUrl) {
+				$(this).show().siblings('.RuoYi_iframe').hide();
+				return false;
+			}
 		});
 	}
 }
@@ -219,78 +266,117 @@ var closeItem = function(dataId){
 /** 创建选项卡 */
 function createMenuItem(dataUrl, menuName) {
 	var panelUrl = window.frameElement.getAttribute('data-id');
-    dataIndex = $.common.random(1,100),
-    flag = true;
-    if (dataUrl == undefined || $.trim(dataUrl).length == 0) return false;
-    var topWindow = $(window.parent.document);
-    // 选项卡菜单已存在
-    $('.menuTab', topWindow).each(function() {
-        if ($(this).data('id') == dataUrl) {
-            if (!$(this).hasClass('active')) {
-                $(this).addClass('active').siblings('.menuTab').removeClass('active');
-                $('.page-tabs-content').animate({ marginLeft: ""}, "fast");
-                // 显示tab对应的内容区
-                $('.mainContent .RuoYi_iframe', topWindow).each(function() {
-                    if ($(this).data('id') == dataUrl) {
-                        $(this).show().siblings('.RuoYi_iframe').hide();
-                        return false;
-                    }
-                });
-            }
-            flag = false;
-            return false;
-        }
-    });
-    // 选项卡菜单不存在
-    if (flag) {
-        var str = '<a href="javascript:;" class="active menuTab" data-id="' + dataUrl + '" data-panel="' + panelUrl + '">' + menuName + ' <i class="fa fa-times-circle"></i></a>';
-        $('.menuTab', topWindow).removeClass('active');
+	dataIndex = $.common.random(1, 100),
+		flag = true;
+	if (dataUrl == undefined || $.trim(dataUrl).length == 0) return false;
+	var topWindow = $(window.parent.document);
+	// 选项卡菜单已存在
+	$('.menuTab', topWindow).each(function() {
+		if ($(this).data('id') == dataUrl) {
+			if (!$(this).hasClass('active')) {
+				$(this).addClass('active').siblings('.menuTab').removeClass('active');
+				scrollToTab(this);
+				$('.page-tabs-content').animate({ marginLeft: ""}, "fast");
+				// 显示tab对应的内容区
+				$('.mainContent .RuoYi_iframe', topWindow).each(function() {
+					if ($(this).data('id') == dataUrl) {
+						$(this).show().siblings('.RuoYi_iframe').hide();
+						return false;
+					}
+				});
+			}
+			flag = false;
+			return false;
+		}
+	});
+	// 选项卡菜单不存在
+	if (flag) {
+		var str = '<a href="javascript:;" class="active menuTab" data-id="' + dataUrl + '" data-panel="' + panelUrl + '">' + menuName + ' <i class="fa fa-times-circle"></i></a>';
+		$('.menuTab', topWindow).removeClass('active');
 
-        // 添加选项卡对应的iframe
-        var str1 = '<iframe class="RuoYi_iframe" name="iframe' + dataIndex + '" width="100%" height="100%" src="' + dataUrl + '" frameborder="0" data-id="' + dataUrl + '" data-panel="' + panelUrl + '" seamless></iframe>';
-        $('.mainContent', topWindow).find('iframe.RuoYi_iframe').hide().parents('.mainContent').append(str1);
-        
-        window.parent.$.modal.loading("数据加载中，请稍后...");
-        $('.mainContent iframe:visible', topWindow).load(function () {
-        	window.parent.$.modal.closeLoading();
-        });
+		// 添加选项卡对应的iframe
+		var str1 = '<iframe class="RuoYi_iframe" name="iframe' + dataIndex + '" width="100%" height="100%" src="' + dataUrl + '" frameborder="0" data-id="' + dataUrl + '" data-panel="' + panelUrl + '" seamless></iframe>';
+		$('.mainContent', topWindow).find('iframe.RuoYi_iframe').hide().parents('.mainContent').append(str1);
 
-        // 添加选项卡
-        $('.menuTabs .page-tabs-content', topWindow).append(str);
-    }
-    return false;
+		window.parent.$.modal.loading("数据加载中，请稍后...");
+		$('.mainContent iframe:visible', topWindow).load(function () {
+			window.parent.$.modal.closeLoading();
+		});
+
+		// 添加选项卡
+		$('.menuTabs .page-tabs-content', topWindow).append(str);
+		scrollToTab($('.menuTab.active', topWindow));
+	}
+	return false;
+}
+
+// 滚动到指定选项卡
+function scrollToTab(element) {
+	var topWindow = $(window.parent.document);
+	var marginLeftVal = calSumWidth($(element).prevAll()),
+		marginRightVal = calSumWidth($(element).nextAll());
+	// 可视区域非tab宽度
+	var tabOuterWidth = calSumWidth($(".content-tabs", topWindow).children().not(".menuTabs"));
+	//可视区域tab宽度
+	var visibleWidth = $(".content-tabs", topWindow).outerWidth(true) - tabOuterWidth;
+	//实际滚动宽度
+	var scrollVal = 0;
+	if ($(".page-tabs-content", topWindow).outerWidth() < visibleWidth) {
+		scrollVal = 0;
+	} else if (marginRightVal <= (visibleWidth - $(element).outerWidth(true) - $(element).next().outerWidth(true))) {
+		if ((visibleWidth - $(element).next().outerWidth(true)) > marginRightVal) {
+			scrollVal = marginLeftVal;
+			var tabElement = element;
+			while ((scrollVal - $(tabElement).outerWidth()) > ($(".page-tabs-content", topWindow).outerWidth() - visibleWidth)) {
+				scrollVal -= $(tabElement).prev().outerWidth();
+				tabElement = $(tabElement).prev();
+			}
+		}
+	} else if (marginLeftVal > (visibleWidth - $(element).outerWidth(true) - $(element).prev().outerWidth(true))) {
+		scrollVal = marginLeftVal - $(element).prev().outerWidth(true);
+	}
+	$('.page-tabs-content', topWindow).animate({ marginLeft: 0 - scrollVal + 'px' }, "fast");
+}
+
+//计算元素集合的总宽度
+function calSumWidth(elements) {
+	var width = 0;
+	$(elements).each(function() {
+		width += $(this).outerWidth(true);
+	});
+	return width;
 }
 
 // 日志打印封装处理
 var log = {
-    log: function(msg) {
-        console.log(msg);
-    },
-    info: function(msg) {
-        console.info(msg);
-    },
-    warn: function(msg) {
-        console.warn(msg);
-    },
-    error: function(msg) {
-        console.error(msg);
-    }
+	log: function(msg) {
+		console.log(msg);
+	},
+	info: function(msg) {
+		console.info(msg);
+	},
+	warn: function(msg) {
+		console.warn(msg);
+	},
+	error: function(msg) {
+		console.error(msg);
+	}
 };
 
 // 本地缓存处理
 var storage = {
-    set: function(key, value) {
-        window.localStorage.setItem(key, value);
-    },
-    get: function(key) {
-        return window.localStorage.getItem(key);
-    },
-    remove: function(key) {
-        window.localStorage.removeItem(key);
-    },
-    clear: function() {
-        window.localStorage.clear();
-    }
+	set: function(key, value) {
+		window.localStorage.setItem(key, value);
+	},
+	get: function(key) {
+		return window.localStorage.getItem(key);
+	},
+	remove: function(key) {
+		window.localStorage.removeItem(key);
+	},
+	clear: function() {
+		window.localStorage.clear();
+	}
 };
 
 // 主子表操作封装处理
@@ -331,15 +417,15 @@ var sub = {
 
 /** 设置全局ajax处理 */
 $.ajaxSetup({
-    complete: function(XMLHttpRequest, textStatus) {
-        if (textStatus == 'timeout') {
-        	$.modal.alertWarning("服务器超时，请稍后再试！");
-        	$.modal.enable();
-            $.modal.closeLoading();
-        } else if (textStatus == "parsererror" || textStatus == "error") {
-        	$.modal.alertWarning("服务器错误，请联系管理员！");
-        	$.modal.enable();
-            $.modal.closeLoading();
-        }
-    }
+	complete: function(XMLHttpRequest, textStatus) {
+		if (textStatus == 'timeout') {
+			$.modal.alertWarning("服务器超时，请稍后再试！");
+			$.modal.enable();
+			$.modal.closeLoading();
+		} else if (textStatus == "parsererror" || textStatus == "error") {
+			$.modal.alertWarning("服务器错误，请联系管理员！");
+			$.modal.enable();
+			$.modal.closeLoading();
+		}
+	}
 });
